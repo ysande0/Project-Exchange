46d42c021bbdb6841ca4453217e35d5bc446a293
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-04-22 16:03:17 -0400

    Add files via upload

---
+++ datautility/FORMATS.java
@@ -0,0 +1,8 @@
+package com.syncadapters.czar.exchange.datautility;
+
+public class FORMATS {
+
+    public static final String time = "%1$tI:%1$tM %Tp";
+    public static final String date = "%1$tm/%1$te/%1$tY";
+
+}
