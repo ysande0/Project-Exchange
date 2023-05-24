ba4b594a669ea41c9529b0e79b5564d91ecf52dc
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-05-24 12:48:29 -0400

    Add files via upload

---
+++ Client/http/ExchangeGlideModule.java
@@ -0,0 +1,12 @@
+package com.syncadapters.czar.exchange.http;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@SuppressWarnings("unused")
+@GlideModule
+public class ExchangeGlideModule extends AppGlideModule {
+
+    
+
+}
