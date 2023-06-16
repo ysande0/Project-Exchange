98efe9ae187b8c25dd73f886bb5fc85f2fd59f7c
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-06-16 17:32:30 -0400

    Add files via upload

---
+++ Server/BoundingBox.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class BoundingBox{
+ 
+    // First Point
+    public $latitude1;
+    public $longitude1;
+    
+    // Second Point
+    public $latitude2;
+    public $longitude2;
+ 
+    // Radius
+    public $radius;
+    
+}
+
+?>
