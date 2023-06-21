503e85be209ee1f3f13120598184087d612febac
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-06-21 18:39:04 -0400

    Update DatabaseLoginInfo.php

--- Server/DatabaseLoginInfo.php
+++ Server/DatabaseLoginInfo.php
@@ -1,15 +1,10 @@
 <?php
-/*
-$database_host = '127.0.0.1'; // Local Machine
-$database_name = 'exchange';
-$database_username = 'root';
+
+//$database_host = ''; // Remote Machine
+$database_host = ''; // Local Machine
+$database_name = '';
+$database_username = '';
 $database_password = '';
-*/
-//$database_host = '64.227.0.161'; // Remote Machine
-$database_host = '127.0.0.1'; // Local Machine
-$database_name = 'exchange_project';
-$database_username = 'czar';
-$database_password = 'GENEsis1!';
 
 
 ?>
