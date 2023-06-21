2ff7a5062a790c4533a359d05640a4720a8e0c0e
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-06-21 18:38:23 -0400

    Add files via upload

---
+++ Server/DatabaseLoginInfo.php
@@ -0,0 +1,15 @@
+<?php
+/*
+$database_host = '127.0.0.1'; // Local Machine
+$database_name = 'exchange';
+$database_username = 'root';
+$database_password = '';
+*/
+//$database_host = '64.227.0.161'; // Remote Machine
+$database_host = '127.0.0.1'; // Local Machine
+$database_name = 'exchange_project';
+$database_username = 'czar';
+$database_password = 'GENEsis1!';
+
+
+?>
