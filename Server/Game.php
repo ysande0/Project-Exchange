f0f7c63f3dc074ddfc505af46012e70719c47198
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-06-29 10:15:43 -0400

    Add files via upload

---
+++ Server/Game.php
@@ -0,0 +1,20 @@
+<?php
+
+class Game{
+    
+    public $title;
+    public $publisher;
+    public $developer;
+    public $platform;
+    public $upc; 
+    public $image_name_thumbnail;
+    public $image_name_full;
+    public $game_image_encoded_thumbnail;
+    public $game_image_encoded_full;
+    public $software_image_thumbnail_url;
+    public $software_image_full_url;
+    public $user_description;
+    public $software_uid;
+    
+    
+}
