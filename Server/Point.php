f4c567353ea516f5d853b900b090df08e7a2f521
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-07-14 15:28:25 -0400

    Add files via upload

---
+++ Server/Point.php
@@ -0,0 +1,18 @@
+<?php
+
+class Point{
+ 
+    public $latitude;
+    public $longitude;
+    
+    public function __construct($latitude, $longitude){
+        
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+        
+    }
+    
+}
+
+
+?>
