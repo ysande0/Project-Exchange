9b508ee0e1f4559d73b0156626bfdd33fde19f0f
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-07-21 16:25:17 -0400

    Update Token.php

--- Server/Token.php
+++ Server/Token.php
@@ -28,46 +28,6 @@ class Token
     {
         $this->token = $token;
     }
-
-    /*
-     * public function validate(){
-     *
-     * $this->read_secret_key();
-     * $does_token_exist = false;
-     * // Check if this user has been assigned this token
-     *
-     * $sql = "SELECT EXISTS (SELECT * FROM tokens WHERE user_id = ? AND token = ?)";
-     * $pdo_statement = $this->pdo->prepare($sql);
-     * $pdo_statement->execute([$this->user_id, $this->token]);
-     * $query_user_id = $pdo_statement->fetch(PDO::FETCH_NUM);
-     *
-     * if($query_user_id[0] == 1)
-     * $does_token_exist = true;
-     * else if($query_user_id[0] == 0)
-     * $does_token_exist = false;
-     *
-     * // print_r("DB STATUS: " . $query_user_id[0] . "\n");
-     *
-     * if($does_token_exist){
-     *
-     * // print_r("This token exists " . "\n");
-     * $is_expired = $this->is_token_expired();
-     *
-     * if($is_expired)
-     * return $is_expired;
-     * else if(!$is_expired)
-     * return $is_expired;
-     *
-     * }
-     * else if(!$does_token_exist){
-     * // print_r("This token does not exist" . "\n");
-     * return true;
-     * }
-     *
-     *
-     *
-     * }
-     */
      
     public function refresh_token()
     {
@@ -88,29 +48,10 @@ class Token
         
         // print_r("The key: " . $this->key);
         $new_token = JWT::encode($payload, $this->key);
-        /*
-        $sql = "UPDATE tokens SET token = ?, token_issue_time = ?, token_start_valid_time = ?, token_end_valid_time = ? WHERE user_id = ?";
-        $pdo_statement = $this->pdo->prepare($sql);
-        $pdo_statement->execute([
-            $new_token,
-            $token_time_issued,
-            $token_start_valid_time,
-            $token_end_valid_time,
-            $this->user_id
-        ]);
-        */
         return $new_token;
     
     }
 
-    /*
-     * public function create_token_table_entry(){
-     *
-     * $sql = "INSERT INTO tokens (token, token_issue_time, token_start_valid_time, token_end_valid_time) VALUES (0, 0, 0, 0)";
-     * $pdo_statement = $this->pdo->prepare($sql);
-     * $pdo_statement->execute();
-     * }
-     */
     public function is_token_expired()
     {
         $this->read_secret_key();
@@ -137,7 +78,6 @@ class Token
     private function read_secret_key()
     {
         $path = '/var/www/exchange_project/';
-       // $path = 'C:/Web/Project/TheExchange Project/';
         $file_path = $path . "keys/exchange_key.txt";
         
         $this->key = file_get_contents($file_path);
