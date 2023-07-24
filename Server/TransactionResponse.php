e34b6154c43e0177ad551a09c404a5ba805eb8ea
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-07-24 18:08:53 -0400

    Add files via upload

---
+++ Server/TransactionResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+class TransactionResponse{
+    
+    public $from_first_name;
+    public $from_uid;
+    public $transaction_id;
+    public $conversation_id;
+    public $user;
+    public $to_first_name;
+    public $to_uid;
+    public $response;
+    
+    
+}
+
+?>
