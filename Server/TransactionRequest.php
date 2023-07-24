fc4d785a482528ecd79601889e625d1981fa4959
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-07-23 21:23:48 -0400

    Add files via upload

---
+++ Server/TransactionRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class TransactionRequest{
+    
+
+    public $software_have_array = array();
+    public $software_want_array = array();
+    public $conversation_id;
+    public $transaction_id;
+    public $from_uid;
+    public $from_id;
+    public $from_first_name;  
+    public $from_profile_image_url;
+    public $to_first_name;
+    public $to_uid;
+    public $to_id;
+    public $to_id_rating;
+    public $time;
+    public $date;
+    
+}
+
+
+?>
