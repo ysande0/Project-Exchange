0f3b3374b9e579e1e17bec5897687e247faf9c61
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-07-28 14:54:16 -0400

    Add files via upload

---
+++ Server/User.php
@@ -0,0 +1,24 @@
+<?php
+
+class User{
+    
+    public $first_name;
+    public $last_name;
+    public $date_of_birth_ms;
+    public $email;
+    public $password;
+    public $id;
+    public $uid;
+    public $fcm_token;
+    public $access_token;
+    public $is_active;
+    public $profile_image_encode_full;
+    public $profile_image_encode_thumbnail;
+    public $profile_image_thumbnail_url;
+    public $profile_image_full_url;
+    public $profile_image_name_thumbnail;
+    public $profile_image_name_full;
+    public $is_email_verified; 
+}
+
+?>
