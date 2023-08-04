439b70df0215eeae34068b2b3b7382db5a55b3c3
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-08-03 20:02:44 -0400

    Update DatabaseLoginInfo.php

--- Server/DatabaseLoginInfo.php
+++ Server/DatabaseLoginInfo.php
@@ -1,7 +1,6 @@
 <?php
 
-//$database_host = ''; // Remote Machine
-$database_host = ''; // Local Machine
+$database_host = ''; 
 $database_name = '';
 $database_username = '';
 $database_password = '';
