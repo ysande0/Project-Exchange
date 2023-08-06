9f2d9b7b99c3c34ea38aa8b81490c0aca3994f84
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-08-06 16:11:07 -0400

    Update Home.php

--- Server/Home.php
+++ Server/Home.php
@@ -1,25 +1,11 @@
 <?php
 $path = '/var/www/exchange_project/';
-//$path = 'C:/Web/Project/TheExchange Project/';
 $private_crypt_key_path = $path . "keys/cryp_key.txt";
 
 require_once 'DatabaseLoginInfo.php';
 require_once 'BoundingBox.php';
 require $path . 'vendor/autoload.php';
 require 'Token.php';
-
-/*
-$array = array();
-$array = $input[0];
-$input = file_get_contents("php://input");
-$input = json_decode($input, true);
-$user_id = $input['id'];
-$user_uid = $input['uid'];
-//$user_token = $array['access_token'];
-$user_latitude  = $input['latitude'];
-$user_longitude = $input['longitude'];
-$miles = $input['radius'];
-*/
 
 $input = file_get_contents("php://input");
 $input = json_decode($input, true);
@@ -31,10 +17,7 @@ $headers = apache_request_headers();
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     
     $user_id = $_GET['id'];
-//$user_uid = $_GET['uid'];
 
-//$user_token = $_GET['access_token'];
-//$user_token = $headers['Authorization'];
 $user_latitude  = $_GET['latitude'];
 $user_longitude = $_GET['longitude'];
 $miles = $_GET['radius'];
@@ -44,58 +27,12 @@ $miles = $_GET['radius'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     
-    //print_r("Updating Initiating");
     $user_id = $input['id'];
     
-   // $user_token = $headers['Authorization'];
     $user_latitude  = $input['latitude'];
     $user_longitude = $input['longitude'];
-    //$miles = $input['radius'];
     
 }
-
-
-
-
-/*
-try{
-    
-    
-    $pdo = new PDO("mysql:host=$database_host;dbname=$database_name", $database_username, $database_password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-     $token = new Token($pdo);
-     $token->set_user_id($user_id);
-     $token->set_token($user_token);
-     $session_timeout = $token->is_token_expired();
-     
-     
-     if($session_timeout){
-     
-     echo json_encode(array(array("session_timeout" => true)));
-     return;
-     
-     }
-
-}catch(PDOException $pdo_error){
-    
-    echo json_encode(array(array("home_error_102" => "error: Database Connection")));
-    $pdo = null;
-    return;
-    
-}
-*/
-/*
-$bounding_box = new BoundingBox();
-$bounding_box->radius = $miles;
-
-$bounding_box->latitude1 =   $user_latitude - ($bounding_box->radius / 69);
-$bounding_box->latitude2 =   $user_latitude + ($bounding_box->radius / 69);
-
-$bounding_box->longitude1 =  $user_longitude - $bounding_box->radius / abs(cos(deg2rad($user_latitude)) * 69);
-$bounding_box->longitude2 =  $user_longitude + $bounding_box->radius / abs(cos(deg2rad($user_latitude)) * 69);
-*/
-//print_r($bounding_box->latitude1 . " " . $bounding_box->longitude1 . " , " . $bounding_box->latitude1 . " " . $bounding_box->longitude2 . ", " . $bounding_box->latitude2 . " " . $bounding_box->longitude2 . "," . $bounding_box->latitude2 . " " . $bounding_box->longitude1 . "," . $bounding_box->latitude1 . " " . $bounding_box->latitude1);
 
 $query;
 try{
@@ -103,29 +40,15 @@ try{
 
     $pdo = new PDO("mysql:host=$database_host;dbname=$database_name", $database_username, $database_password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-/*
-    $token = new Token($pdo);
-    $token->set_user_id($user_id);
-    $token->set_token($user_token);
-    $session_timeout = $token->is_token_expired();
-    
 
-    if($session_timeout){
-        
-        echo json_encode(array(array("session_timeout" => true)));
-        return;
-    }
-*/
     
     if($_SERVER['REQUEST_METHOD'] === 'PUT'){
         
-       // print_r("Updating executing");
-        //$sql = "UPDATE locations SET position = ST_GEOMFROMTEXT('POINT(? ?)') WHERE user_id = ?";
+
         $sql = "UPDATE locations SET position = (POINT(? , ?)) WHERE user_id = ?";
         $pdo_statement = $pdo->prepare($sql);
         $is_executed = $pdo_statement->execute([$user_latitude, $user_longitude, $user_id]);
-       // print_r("Updating executed");
+
         
         if(!$is_executed){
             
@@ -179,13 +102,7 @@ try{
         }
         
         $query = $pdo_statement->fetchAll(PDO::FETCH_ASSOC);
-    
-        /*
-        $soft_ttl = 3600000;
-        $hard_ttl = 86400000;
-    
-       header("Cache-Control: max-age=$hard_ttl"); // <--- the TTL/Hard TTL
-    */
+
       echo json_encode($query);
      
     }
