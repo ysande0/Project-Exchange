8b7eca985089d6871fd7ba29a4133e5303b0ba6a
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2023-09-13 20:11:43 -0400

    Update HardwareInventoryActivity.java

--- Client/activities/HardwareInventoryActivity.java
+++ Client/activities/HardwareInventoryActivity.java
@@ -36,13 +36,7 @@ public class HardwareInventoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hardware_inventory);
 
-        // Read database. If null then get input. If Non null, then set switch to true
-        Log.d(TAG, "HardwareInventoryActivity onCreate");
-
-        // Query database items
-        // Load database items in recycle view
-        // Remove any items in database that are already in database
-
+        
         LinearLayout hardware_display_section_linear_layout = findViewById(R.id.hardware_display_section_linear_layout_id);
         hardware_platform_recycle_view = findViewById(R.id.hardware_platform_recycleView_id);
         ProgressBar progress_bar = findViewById(R.id.hardware_progress_bar_circular_id);
@@ -55,9 +49,7 @@ public class HardwareInventoryActivity extends AppCompatActivity {
 
         //noinspection ConstantConditions
         if(getIntent().getExtras().getBoolean(getResources().getString(R.string.is_beginner_key))) {
-            Log.d(TAG, "HardwareActivity: is Beginner ");
             is_beginner = true;
-
         }
 
         ActionBar action_bar = getSupportActionBar();
@@ -73,7 +65,7 @@ public class HardwareInventoryActivity extends AppCompatActivity {
         }
 
         continue_action_button.setOnClickListener(v -> alert_dialog_box());
-       // HardwareInventoryViewModel hardware_inventory_view_model = ViewModelProviders.of(this).get(HardwareInventoryViewModel.class);
+        
         HardwareInventoryViewModel hardware_inventory_view_model = new ViewModelProvider(this).get(HardwareInventoryViewModel.class);
         hardware_inventory_view_model.query_hardware(HardwareInventoryActivity.this, hardware_platform_recycle_view, hardware_platform_spinner,
                 add_hardware_platform_button, progress_bar, hardware_display_section_linear_layout, no_hardware_available_textView, continue_action_button);
@@ -98,56 +90,9 @@ public class HardwareInventoryActivity extends AppCompatActivity {
 
     }
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater menu_inflater = getMenuInflater();
-        menu_inflater.inflate(R.menu.hardware_inventory_activity_menu_items, menu);
-        return true;
-
-    }
-
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch(item.getItemId()){
-
-            case R.id.profile_settings_item_id:
-                launch_profile_settings_activity();
-                return true;
-
-            case R.id.profile_logout_item_id:
-                launch_profile_logout();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
-
-    }
-
-    private void launch_profile_settings_activity(){
-
-        Intent intent = new Intent(HardwareInventoryActivity.this, ProfileSettingsActivity.class);
-        startActivity(intent);
-
-    }
-
-    private void launch_profile_logout(){
-
-       hardware_inventory_view_model.logout(app, HardwareInventoryActivity.this);
-
-    }
-*/
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "HardwareInventoryActivity onDestroy");
 
         hardware_platform_recycle_view.setAdapter(null);
 
