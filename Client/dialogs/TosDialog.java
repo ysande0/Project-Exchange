3fe083ac6fac8801b22f82666d0ad7e7e1307c33
Yankho Sande <130524360+ysande0@users.noreply.github.com>
2024-04-14 01:22:00 -0400

    Update TosDialog.java

--- Client/dialogs/TosDialog.java
+++ Client/dialogs/TosDialog.java
@@ -31,11 +31,6 @@ public class TosDialog extends DialogFragment {
         ImageView exit_imageView = view.findViewById(R.id.tos_dialog_clear_image_imageView_id);
 
 
-/*
-        TextView terms_of_service_textView = view.findViewById(R.id.terms_of_service_text_view_id);
-        String terms_of_service_document = getResources().getString(R.string.manaphest_terms_of_service);
-        terms_of_service_textView.setText(Html.fromHtml(terms_of_service_document));
-*/
         exit_imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
